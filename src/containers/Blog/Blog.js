4bbc65642f2427f624295930541c6a6275c2a140
CodeSevenic <sbu.audio.production@gmail.com>
2021-03-07 17:16:08 +0200

    first commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
+
+import classes from './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className={classes.Blog}>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true',
+                  }}
+                >
+                  New Post
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home II</h1>} /> */}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
+      </div>
+    );
+  }
+}
+
+export default Blog;
