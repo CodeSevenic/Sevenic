200161ba6578116f168d115e219be1ac340b22b2
CodeSevenic <sbu.audio.production@gmail.com>
2021-03-09 10:36:05 +0200

    my changes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,6 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import classes from './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
   render() {
@@ -21,10 +20,10 @@ class Blog extends Component {
                   //   color: 'blue',
                   //   textDecoration: 'underline',
                   // }}
-                  to="/"
+                  to="/posts/"
                   exact
                 >
-                  Home
+                  Posts
                 </NavLink>
               </li>
               <li>
@@ -43,9 +42,8 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path="/" exact component={Posts} />
-          <Route path="/new-post" exact component={NewPost} />
-          <Route path="/:id" exact component={FullPost} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/posts/" component={Posts} />
         </Switch>
       </div>
     );
